05fa8b326396e2b34d759ad99e7b688536178109
hadi-torfi <photo.love512@gmail.com>
2022-01-25 15:35:16 +0330

    create packages

---
+++ app/src/main/java/com/haditorfi/dictionary/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.haditorfi.dictionary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.haditorfi.dictionary.ui.theme.DictionaryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DictionaryTheme {
+            }
+        }
+    }
+}
